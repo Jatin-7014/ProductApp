06220434b221f9c472903f5ca014fa12e887d045
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-12 18:22:53 +0530

    Initial Commit

---
+++ Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Models
+{
+    internal class Product
+    {
+        int _id;
+        string _name;
+        int _price;
+        int _discount;
+        public int GetId() { return _id; }
+
+        public int GetPrice() { return _price; }
+
+        public int GetDiscount() { return _discount; }
+
+        public string GetName() { return _name; }
+        
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+
+        public void SetPrice(int price)
+        {
+            _price = price;
+        }
+
+        public void SetDiscount(int discount) 
+        { 
+            _discount = discount; 
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public int SellingPrice()
+        {
+            int discount = _price - (_price * _discount / 100);
+            return discount;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Product shirt = new Product();
+            shirt.SetPrice(1500);
+            shirt.SetName("US POLO");
+            shirt.SetId(101);
+            shirt.SetDiscount(20);
+
+            Product shoes = new Product();
+            shoes.SetPrice(2500);
+            shoes.SetName("Nike");
+            shoes.SetId(201);
+            shoes.SetDiscount(15);
+
+            void showDetails(Product name)
+            {
+                Console.WriteLine("Id: "+name.GetId());
+                Console.WriteLine("Discount Applied :"+name.GetDiscount()+" percentage");
+                Console.WriteLine("Product name: "+name.GetName());
+                Console.WriteLine("Actual Price :"+name.GetPrice());
+                Console.WriteLine("Selling price after discount: "+name.SellingPrice());
+            }
+            Console.WriteLine("Details of Product 1: \n");
+            showDetails(shirt);
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Details of Product 2: \n");
+            showDetails(shoes);
+            
+        }
+    }
+}
